9e21693f030a38b2e73f00fa7cfa2b517297b3dd
benjamin gonzalez <jgonzalez.benjamin@gmail.com>
2024-01-10 08:58:04 -0300

    agregado de variables scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IntroComponent } from './views/intro/intro.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, IntroComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'benjapp';
+  title = 'benjamin gonzalez - dev front-end';
 }
